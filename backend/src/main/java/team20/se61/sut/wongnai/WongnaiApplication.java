8222ff18fd79fcdc5c78b870933d7d9e9c6c77b3
suriya13052540 <suriyathailandcpe@gmail.com>
2018-12-19 18:06:07 +0700

    add backend

---
+++ backend/src/main/java/team20/se61/sut/wongnai/WongnaiApplication.java
@@ -0,0 +1,14 @@
+package team20.se61.sut.wongnai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WongnaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WongnaiApplication.class, args);
+	}
+
+}
+
---
+++ backend/src/test/java/team20/se61/sut/wongnai/WongnaiApplicationTests.java
@@ -0,0 +1,17 @@
+package team20.se61.sut.wongnai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WongnaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
