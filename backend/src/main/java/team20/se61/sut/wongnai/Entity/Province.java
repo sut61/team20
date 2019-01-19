c2bbd8e5ba6cb802c102f3db44f7822090c02627
yoyoland <yamaha.yoyoland@gmail.com>
2019-01-20 00:20:55 +0700

    close #51 -เพิ่มไฟล์Entity

---
+++ backend/src/main/java/team20/se61/sut/wongnai/Entity/Province.java
@@ -0,0 +1,17 @@
+package team20.se61.sut.wongnai.Entity;
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Getter @Setter
+public class Province {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    public Province(){}
+
+
+}
