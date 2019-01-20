1b74e09f6fd1aba42d025771c6c4b9a63292f756
ouujj <saranyu235@gmail.com>
2019-01-20 23:28:37 +0700

    add old file

--- backend/src/main/java/team20/se61/sut/wongnai/Entity/Business.java
+++ backend/src/main/java/team20/se61/sut/wongnai/Entity/Business.java
@@ -1,7 +1,10 @@
 package team20.se61.sut.wongnai.Entity;
+
 import lombok.*;
 
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Entity;
 
@@ -10,5 +13,22 @@ import javax.persistence.Entity;
 public class Business {
     @Id @GeneratedValue
     private Long id;
+    private String shopName;
+    private String district;
+    private String tel;
+    private String email;
+
     public Business(){}
+
+    @ManyToOne()   
+    @JoinColumn(name= "provinceId")     
+    private Province province;
+
+    @ManyToOne()   
+    @JoinColumn(name= "profilesId")     
+    private ProfilesEntity profile;
+
+    @ManyToOne()   
+    @JoinColumn(name= "typeId")     
+    private BusinessType type;
 }
