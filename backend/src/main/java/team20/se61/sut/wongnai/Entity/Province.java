6c67a859bbe3e29ff461a604b23de4bee8ef01d0
yoyoland <yamaha.yoyoland@gmail.com>
2019-01-22 01:13:47 +0700

    close #68 -เพิ่ม sequence

--- backend/src/main/java/team20/se61/sut/wongnai/Entity/Province.java
+++ backend/src/main/java/team20/se61/sut/wongnai/Entity/Province.java
@@ -1,14 +1,19 @@
 package team20.se61.sut.wongnai.Entity;
+
 import lombok.*;
 
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 
 @Entity
 @Getter @Setter
 public class Province {
-    @Id @GeneratedValue
+    @Id
+    @SequenceGenerator(name="Province_seq",sequenceName="Province_seq")               
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="Province_seq")
     private Long id;
     private String name;
     public Province(){}
