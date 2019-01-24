307dda989d3ca3da2d90074de15a7aaa85ddb031
coregatekit <joesm.genie@gmail.com>
2019-01-25 01:35:14 +0700

    close #73 เพิ่ม Dataloader สำหรับระบบสั่งซื้ออาหาร

---
+++ backend/src/main/java/team20/se61/sut/wongnai/DataLoader/FoodDataloader.java
@@ -0,0 +1,110 @@
+package team20.se61.sut.wongnai.DataLoader;
+import team20.se61.sut.wongnai.Entity.*;
+import team20.se61.sut.wongnai.Repository.*;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class FoodDataloader implements ApplicationRunner {
+
+    @Autowired private ProvinceRepository provinceRepository;
+    @Autowired private BusinessTypeRepository businessTypeRepository;
+    @Autowired private BusinessRepository businessRepository;
+    @Autowired private CookingMethodRespository cookingMethodRespository;
+    @Autowired private FoodTypeRespository foodTypeRespository;
+    @Autowired private MainIngredRespository mainIngredRespository;
+    @Autowired private RecipeRespository recipeRespository;
+    @Autowired private PriceRangeRepository priceRangeRepository;
+    @Autowired private NumberOfSeatRepository numberOfSeatRepository;
+    @Autowired private ContactRepository contactRepository;
+    @Autowired private PrefixRepository prefixRepository;
+    @Autowired private SexRepository sexRepository;
+    @Autowired private ProfilesRepository profilesRepository;
+    @Autowired private StoreRepository storeRepository;
+    @Autowired private FoodRepository foodRepository;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        ContactEntity c1 = new ContactEntity();
+        c1.setAddress("address");
+        c1.setTelephonenumber("0999999999");
+        contactRepository.save(c1);
+
+        PrefixEntity prefix = new PrefixEntity();
+        prefix.setPrefix("อื้ม");
+        prefixRepository.save(prefix);
+
+        SexEntity sex = new SexEntity();
+        sex.setSex("เพศที่สาม");
+        sexRepository.save(sex);
+
+        NumberOfSeat numSeat = new NumberOfSeat("500");
+        numberOfSeatRepository.save(numSeat);
+
+        Province province = new Province();
+        province.setName("Sydney");
+        provinceRepository.save(province);
+
+        BusinessType newBusinessType = new BusinessType();
+        newBusinessType.setName("ขายอยู่ มทส.");
+        businessTypeRepository.save(newBusinessType);
+
+        ProfilesEntity p1 = new ProfilesEntity();
+        p1.setContact(c1);
+        p1.setEmail("www@www.gg");
+        p1.setName("อื้ม งืมๆ");
+        p1.setPassword("11111111");
+        p1.setPrefix(prefix);
+        p1.setSex(sex);
+        profilesRepository.save(p1);
+
+        Business business = new Business();
+        business.setProfile(p1);
+        business.setDistrict("district");
+        business.setEmail("email");
+        business.setProvince(province);
+        business.setShopName("shopName");
+        business.setTel("0888888888");
+        business.setType(newBusinessType);
+        businessRepository.save(business);
+
+        Store st = new Store();
+        st.setAdddress("adddress");
+        st.setBranch("branch");
+        st.setBuilding("building");
+        st.setBusiness(business);
+        st.setDistrict("district");
+        st.setEmail("email");
+        st.setHint("hint");
+        st.setImage("https://vignette.wikia.nocookie.net/redvelvet/images/f/f6/The_Red_Summer_Logo.png/revision/latest?cb=20170709132135&format=original");
+        st.setName("name");
+        st.setNumberOfSeat(numSeat);
+        st.setPhone("0000000000");
+        storeRepository.save(st);
+
+        String[] foodName = {"เบคอนกรอบ", "น้ำตกไก่", "ฮาวายเอี้ยน", "ซีฟู้ดค็อกเทล", "ซุปเปอร์เดอลุกซ์"};
+        String[] foodImage = {"https://1112.minorcdn.com/1112/public/images/products/pizza/website/Pizza-Pan-Crispy-Bacon_New-Toppings_Angle_web.png",
+        "https://1112.minorcdn.com/1112/public/images/products/pizza/website/Pizza-Pan-Spicy-Namtok-Chicken_New-Toppings_Angle_web.png",
+        "https://1112.minorcdn.com/1112/public/images/products/pizza/website/Pan_Hawaiian.png",
+        "https://1112.minorcdn.com/1112/public/images/products/pizza/website/Pan_Seafood-Cocktail.png",
+        "https://1112.minorcdn.com/1112/public/images/products/pizza/website/Pan_Super-Deluxe.png"};
+        int[] foodPrice = {125,150,175,200,250};
+
+        for (int i = 0; i<5; i++) {
+            Food food = new Food();
+            food.setName(foodName[i]);
+            food.setStore(st);
+            food.setImage(foodImage[i]);
+            food.setPrice(foodPrice[i]);
+            foodRepository.save(food);
+        }
+
+
+        
+        provinceRepository.findAll().forEach(System.out::println);
+        businessTypeRepository.findAll().forEach(System.out::println);
+	}
+    
+}
