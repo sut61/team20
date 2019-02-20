469bec4560eee0a535028cf27bffa302860aa938
Miwtoo <miwyugioh@gmail.com>
2019-02-21 00:29:20 +0700

    close #147 - แก้ไขตัวแปร

--- backend/src/main/java/team20/se61/sut/wongnai/Entity/NumberOfSeat.java
+++ backend/src/main/java/team20/se61/sut/wongnai/Entity/NumberOfSeat.java
@@ -3,6 +3,7 @@ package team20.se61.sut.wongnai.Entity;
 import lombok.*;
 import javax.persistence.*;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 
@@ -13,6 +14,7 @@ public class NumberOfSeat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String choices;
 
     protected NumberOfSeat(){}
