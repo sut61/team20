0af60235e08c64eb00ad48a504edea051b949a21
coregatekit <joesm.genie@gmail.com>
2019-02-22 16:17:54 +0700

    close #154 - update all selenium test

--- frontend/src/app/order-confirm/order-confirm.component.ts
+++ frontend/src/app/order-confirm/order-confirm.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../shared/login/login.service';
-import { Router } from '@angular/router';
-import { OrderService } from '../shared/order/order.service';
-import { database } from 'firebase';
+import { Component, OnInit } from "@angular/core";
+import { LoginService } from "../shared/login/login.service";
+import { Router } from "@angular/router";
+import { OrderService } from "../shared/order/order.service";
+import { database } from "firebase";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-order-confirm',
-  templateUrl: './order-confirm.component.html',
-  styleUrls: ['./order-confirm.component.css']
+  selector: "app-order-confirm",
+  templateUrl: "./order-confirm.component.html",
+  styleUrls: ["./order-confirm.component.css"]
 })
 export class OrderConfirmComponent implements OnInit {
-
   foodId;
   public foodDetail: any;
   orderDetail: any = {
-    uid: '',
-    sid: '',
-    fid: '',
-    countItem: '',
+    uid: "",
+    sid: "",
+    fid: "",
+    countItem: "",
     totalPrice: 0,
-    addr: ''
+    addr: ""
   };
 
   selectedValue: string;
@@ -29,37 +29,38 @@ export class OrderConfirmComponent implements OnInit {
   userAddress: string;
   storeId: string;
 
-
   profile: any;
   order: any;
 
- 
-
-  constructor(private orderService: OrderService, private loginService: LoginService, private  router :Router) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private orderService: OrderService,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.foodId = localStorage.getItem("food");
-    console.log("Foodd Id: "+this.foodId);
+    console.log("Foodd Id: " + this.foodId);
 
     this.getFoodById();
 
-    this.storeId = localStorage.getItem('store');
+    this.storeId = localStorage.getItem("store");
     console.log("store", this.storeId);
 
     this.loginService.getUser().subscribe(data => {
-      try{
+      try {
         this.email = data.email;
         this.userId = data.profilesid;
         this.userAddress = data.contact.address;
-        console.log(this.email)
-        console.log(this.userId)
-        console.log(this.userAddress)
-       }
-       catch(Err){
-         this.router.navigate(['/login']);
-       }
+        console.log(this.email);
+        console.log(this.userId);
+        console.log(this.userAddress);
+      } catch (Err) {
+        this.router.navigate(["/login"]);
+      }
     });
-    console.log(this.orderDetail.addr)
+    console.log(this.orderDetail.addr);
   }
 
   getFoodById() {
@@ -72,44 +73,52 @@ export class OrderConfirmComponent implements OnInit {
   }
 
   onKey(event: KeyboardEvent) {
-    try{
-      this.orderDetail.totalPrice = this.foodDetail.price * this.orderDetail.countItem;
-    }
-    catch{
-      console.log('กรุณากรอกตัวเลข');
+    try {
+      this.orderDetail.totalPrice =
+        this.foodDetail.price * this.orderDetail.countItem;
+    } catch {
+      console.log("กรุณากรอกตัวเลข");
     }
   }
-
 
   confirm() {
     this.setOrder();
-    console.log("orderDetail = ", this.orderDetail)
-    this.orderService.addOrder(this.orderDetail).subscribe(data => {
-      alert('บันทึกรายการสำเร็จ')
-      this.orderDetail.uid = '',
-      this.orderDetail.sid = '',
-      this.orderDetail.fid = '',
-      this.orderDetail.countItem = '',
-      this.orderDetail.totalPrice = 0,
-      this.orderDetail.addr= ''
-      localStorage.clear();
-      this.router.navigate(['/manu']);
-    },
-    error => {
-      alert('ไม่สามารถทำรายการได้')
-    });
+    console.log("orderDetail = ", this.orderDetail);
+    this.orderService.addOrder(this.orderDetail).subscribe(
+      data => {
+        this.snackBar.open("บันทึกรายการสำเร็จ ", "ตกลง", {
+          duration: 10000,
+          verticalPosition: "top",
+          horizontalPosition: "center"
+        });
+        (this.orderDetail.uid = ""),
+          (this.orderDetail.sid = ""),
+          (this.orderDetail.fid = ""),
+          (this.orderDetail.countItem = ""),
+          (this.orderDetail.totalPrice = 0),
+          (this.orderDetail.addr = "");
+        localStorage.clear();
+        this.router.navigate(["/manu"]);
+      },
+      error => {
+        this.snackBar.open("ไม่สามารถทำรายการได้ ", "ตกลง", {
+          duration: 10000,
+          verticalPosition: "top",
+          horizontalPosition: "center"
+        });
+      }
+    );
   }
 
   clear() {
-    this.orderDetail.countItem = '',
-    this.orderDetail.totalPrice = 0
-    this.selectedValue = ''
+    (this.orderDetail.countItem = ""), (this.orderDetail.totalPrice = 0);
+    this.selectedValue = "";
   }
 
   goToMenu() {
     this.clear();
     localStorage.clear();
-    this.router.navigate(['/manu']);
+    this.router.navigate(["/manu"]);
   }
 
   setOrder() {
@@ -117,5 +126,4 @@ export class OrderConfirmComponent implements OnInit {
     this.orderDetail.sid = this.storeId;
     this.orderDetail.addr = this.selectedValue;
   }
-
 }
